fdb6571a2311760be9dc00cdc9d264cabb1910b7
hackeroid080819 <53891847+hackeroid080819@users.noreply.github.com>
2019-09-02 16:22:13 +0300

    Update Req_snippet.java

--- WebServiceAdv/Req_snippet.java
+++ WebServiceAdv/Req_snippet.java
@@ -1,9 +1,20 @@
         String url = "http://10.0.2.2:8080/webapi/coupon";
+        /*  if you get an ERROR - you may need to change the manifest:
+        <manifest ...>
+                <uses-permission android:name="android.permission.INTERNET" />
+                        <application
+                        ...
+                        android:usesCleartextTraffic="true"
+                        ...>
+                        ...
+                </application>
+        </manifest> */
 
         Request request = new Request.Builder()
                 .url(url)
                 .build();
-        // POST
+
+        // for - POST
         //MediaType JSON = MediaType.parse("application/json; charset=utf-8");
         //RequestBody body = RequestBody.create(JSON, "{\"id\":1, \"value\":\"hello\"}");
         //Request request = new Request.Builder()
